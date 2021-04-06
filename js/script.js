3c9d83dd7f4faada1d78a85d5603ca7992261db2
aoshiro <koh.sss0629@gmail.com>
2021-04-06 20:13:10 +0900

    form サンキューページへのレンダリング　追加

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ $(function() {
 		$('.start').fadeOut(1000);
 	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
+
+/* ===============================================*/
+/* フォームバリデーション
+/*=============================================== */
+(function() {
+	window.addEventListener('load', function() {
+		var forms = document.getElementsByClassName('needs-validation');
+		var validation = Array.prototype.filter.call(forms, function(form) {
+			form.addEventListener('submit', function(event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add('was-validated');
+			}, false);
+		});
+	}, false);
+})();
